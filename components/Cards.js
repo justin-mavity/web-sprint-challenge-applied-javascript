ca3f2d093f375cb179b427f755343fd031867e25
justin-mavity <justinmavity1021@gmail.com>
2020-12-04 10:57:08 -0600

    finished

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,52 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+import axios from "axios";
+axios
+  .get("https://lambda-times-api.herokuapp.com/articles")
+  .then((res) => {
+    const mainTopics = Object.entries(res.data.articles);
+    mainTopics.forEach((article) => {
+      article[1].forEach((data) => {
+        const newCard = cardMaker({
+          headline: data.headline,
+          imageURL: data.authorPhoto,
+          authorName: data.authorName,
+        });
+        document.querySelector(".cards-container").appendChild(newCard);
+      });
+    });
+  })
+  .catch((err) => {
+    return alert(err);
+  });
+
+function cardMaker({ headline, imageURL, authorName }) {
+  const card = document.createElement("div");
+  const heading = document.createElement("div");
+  const author = document.createElement("div");
+  const imageCon = document.createElement("div");
+  const image = document.createElement("img");
+  const name = document.createElement("span");
+
+  card.classList.add("card");
+  heading.classList.add("headline");
+  author.classList.add("author");
+  imageCon.classList.add("img-container");
+
+  heading.textContent = headline;
+  image.src = imageURL;
+  name.textContent = authorName;
+
+  card.appendChild(heading);
+  card.appendChild(author);
+  author.appendChild(imageCon);
+  author.appendChild(name);
+  imageCon.appendChild(image);
+
+  card.addEventListener("click", () => {
+    console.log(heading);
+  });
+
+  return card;
+}
--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,19 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+import axios from "axios";
+axios
+  .get("https://lambda-times-api.herokuapp.com/topics")
+  .then((res) => {
+    const topic = res.data.topics;
+    topic.forEach((elem) => {
+      const tab = document.createElement("div");
+      tab.classList.add("tab");
+      tab.textContent = elem;
+      console.log(topic);
+      document.querySelector(".topics").appendChild(tab);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
